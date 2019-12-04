cc1a7b138f3a9cff4da4a9358e98ff740a502314
HuangYanX <hyxdd499@163.com>
2019-12-04 10:33:32 +0800

    可以异步操作redux的中间件saga和thunk

--- todolist/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import TodoList from './pages/TodoList'
-ReactDOM.render(
-    <>
-      <TodoList></TodoList> 
-    </>,
-    document.getElementById('root')
-)
